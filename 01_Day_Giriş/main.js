c75fa7158a1ff3979f2d922f0c8a9d3209d7f6bc
berkesasa <berkesasa11@gmail.com>
2023-11-16 18:01:53 +0300

    first upload

---
+++ 01_Day_Giriş/main.js
@@ -0,0 +1,51 @@
+//Egzersiz-1
+//Yorumlar kodu okunabilir hale getirmeye yardımcı olur
+
+//Egzersiz-2
+//30GundeJavascript eğitimine hoş geldiniz
+
+//Egzersiz-3
+/* Yorumlar kodu okunabilir,
+yeniden kullanımı kolay ve
+bilgilendirici bir hale getirir
+*/
+
+//Egzersiz-4
+let stringDegisken = "Berke";
+let booleanDegisken = true;
+let undefinedDegisken;
+let nullDegisken = null;
+
+//Egzersiz-5
+console.log(typeof stringDegisken);
+console.log(typeof booleanDegisken);
+console.log(typeof undefinedDegisken);
+console.log(typeof nullDegisken);
+
+//Egzersiz-6
+let degisken1;
+let degisken2;
+let degisken3;
+let degisken4;
+
+//Egzersiz-7
+degisken1 = "Berke";
+degisken2 = true;
+degisken3 = 3.14;
+degisken4 = null;
+
+//Egzersiz-8
+let ad = "Berke";
+let soyAd = 'Sasa';
+let medeniDurum = `Bekar`;
+let ulke = "Türkiye";
+let yas = 26;
+
+//Egzersiz-9
+let ad2 = "Berke"; let soyAd2 = 'Sasa'; let medeniDurum2 = `Bekar`; let ulke2 = "Türkiye"; let yas2 = 26;
+
+//Egzersiz-10
+let benimYasim = "Ben 25 Yaşındayım"
+let seninYasin = "Ben 30 Yaşındayım";
+console.log(benimYasim, seninYasin); //ya da 
+console.log(benimYasim); console.log(seninYasin);
