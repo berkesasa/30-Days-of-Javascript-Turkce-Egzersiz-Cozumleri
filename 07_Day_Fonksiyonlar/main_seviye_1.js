ee6052c38f214f849e544fbbffc323688fb38f5b
berkesasa <berkesasa11@gmail.com>
2023-12-07 21:14:03 +0300

    Day_7 exercies one half-completed

--- 07_Day_Fonksiyonlar/main_seviye_1.js
+++ 07_Day_Fonksiyonlar/main_seviye_1.js
@@ -1,58 +1,79 @@
 //Seviye-1
 
 /* Egzersiz-1
--Bir fullName fonksiyonu tanımlayın ve tam adınızı yazdırsın.
+-Bir fullName fonksiyonu tanımlayın ve tam adınızı yazdırın.
 */
-
-
+function fullName(){
+    console.log("berke sasa");
+}
+fullName()
 
 /* Egzersiz-2
 -Bir fullName fonksiyonu tanımlayın ve şimdi parametre olarak firstName, lastName alır ve tam adınızı döndürür.
 */
-
-
+function fullName2(firstName, lastName){
+    console.log(firstName + " " + lastName);
+}
+fullName2("berke", "sasa")
 
 /* Egzersiz-3
 -Bir addNumbers fonksiyonu tanımlayın ve bu fonksiyon iki parametre alır ve toplamı döndürür.
 */
-
-
+function addNumbers(numberOne, numberTwo) {
+    return numberOne+numberTwo
+}
+console.log(addNumbers(5,3));
 
 /* Egzersiz-4
 -Bir dikdörtgenin alanı şu şekilde hesaplanır: alan = uzunluk x genişlik. Dikdörtgenin alanını hesaplayan bir fonksiyon yazın.
 */
-
-
+function alanHesaplama(uzunluk, genislik) {
+    return uzunluk*genislik
+}
+console.log(alanHesaplama(5,4));
 
 /* Egzersiz-5
 -Bir dikdörtgenin çevresi şu şekilde hesaplanır: çevre= 2x(uzunluk + genişlik). Dikdörtgenin çevresini hesaplayan bir fonksiyon yazınız.
 */
-
-
+function cevreHesaplama(uzunluk, genislik) {
+    return 2*uzunluk*genislik
+}
+console.log(cevreHesaplama(5,4));
 
 /* Egzersiz-6
 -Bir dikdörtgenler prizmasının hacmi şu şekilde hesaplanır: hacim = uzunluk x genişlik x yükseklik. DikdörtgenPrizma hacmini hesaplayan bir fonksiyon yazınız.
 */
-
-
+function prizmaHacim(uzunluk, genislik, yukseklik) {
+    return uzunluk*genislik*yukseklik
+}
+console.log(prizmaHacim(5,4,3));
 
 /* Egzersiz-7
 -Bir dairenin alanı şu şekilde hesaplanır: alan = π x r x r. Dairenin alanını hesaplayan bir fonksiyon yazınız.
 */
-
-
+function daireAlan(yaricap) {
+    const PI = Math.PI
+    return PI*yaricap*yaricap
+}
+console.log(daireAlan(5));
 
 /* Egzersiz-8
 -Bir dairenin çevresi şu şekilde hesaplanır: çevre = 2πr. Dairenin çevresini hesaplayan bir fonksiyon yazın
 */
-
-
+function daireCevresi(yaricap) {
+    const PI = Math.PI
+    return 2*PI*yaricap
+}
+console.log(daireCevresi(5));
 
 /* Egzersiz-9
 -Bir maddenin yoğunluğu şu şekilde hesaplanır: yoğunluk= kütle/hacim. Yoğunluğu hesaplayan bir fonksiyon yazınız.
 */
-
-
+function daireCevresi(yaricap) {
+    const PI = Math.PI
+    return 2*PI*yaricap
+}
+console.log(daireCevresi(5));
 
 /* Egzersiz-10
 -Hız, hareket eden bir cismin kat ettiği toplam mesafenin geçen toplam süreye bölünmesiyle hesaplanır. Hareket eden bir nesnenin hızını hesaplayan bir fonksiyon yazınız, hız.
