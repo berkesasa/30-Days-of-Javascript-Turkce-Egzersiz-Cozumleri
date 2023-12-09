bc2bd36ea7039aaccc8305c23215579e82310d2e
berkesasa <berkesasa11@gmail.com>
2023-12-09 13:29:02 +0300

    updated exercise 2

--- 07_Day_Fonksiyonlar/main_seviye_2.js
+++ 07_Day_Fonksiyonlar/main_seviye_2.js
@@ -1,158 +1,293 @@
-//Seviye-1
+//Seviye-2
 
 /* Egzersiz-1
--Bir fullName fonksiyonu tanımlayın ve tam adınızı yazdırın.
+-Doğrusal denklem şu şekilde hesaplanır: ax + by + c = 0. Doğrusal denklemin değerini hesaplayan bir fonksiyon yazın, solveLinEquation.
 */
-function fullName() {
-    console.log("berke sasa");
+function solveLinEquation(a, b, c) {
+    if (a === 0 && b === 0) {
+        return "Bu doğrusal bir denklem değildir.";
+    } else if (a === 0) {
+        let y = -c / b
+        return { y: y }
+    } else if (b === 0) {
+        let x = -c / a
+        return { x: x }
+    }
+    var x = -c / a
+    var y = -c / b
+
+    return { x: x, y: y }
 }
-fullName()
+console.log(solveLinEquation(2, -3, 6));
 
 /* Egzersiz-2
--Bir fullName fonksiyonu tanımlayın ve şimdi parametre olarak firstName, lastName alır ve tam adınızı döndürür.
+-İkinci dereceden denklem şu şekilde hesaplanır: ax2 + bx + c = 0. İkinci dereceden denklemin değerini veya değerlerini hesaplayan bir fonksiyon yazın, solveQuadEquation.
+    console.log(solveQuadratic()) // {0}
+    console.log(solveQuadratic(1, 4, 4)) // {-2}
+    console.log(solveQuadratic(1, -1, -2)) // {2, -1}
+    console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
+    console.log(solveQuadratic(1, 0, -4)) //{2, -2}
+    console.log(solveQuadratic(1, -1, 0)) //{1, 0}
 */
-function fullName2(firstName, lastName) {
-    console.log(firstName + " " + lastName);
+function solveQuadEquation(a, b, c) {
+    let diskriminant = (b * b) - 4 * a * c
+
+    if (diskriminant < 0) {
+        return "Reel kok yok";
+    }
+
+    var kok1 = (-b + Math.sqrt(diskriminant)) / (2 * a);
+    var kok2 = (-b - Math.sqrt(diskriminant)) / (2 * a);
+
+    return [kok1, kok2]
 }
-fullName2("berke", "sasa")
+console.log(solveQuadEquation(1, -1, -2));
 
 /* Egzersiz-3
--Bir addNumbers fonksiyonu tanımlayın ve bu fonksiyon iki parametre alır ve toplamı döndürür.
+-printArray adında bir fonksiyon tanımlayın. Parametre olarak dizi alır ve dizinin her bir değerini yazdırır.
 */
-function addNumbers(numberOne, numberTwo) {
-    return numberOne + numberTwo
+function printArray(dizi) {
+    dizi.map((value) => {
+        console.log(value);
+    })
 }
-console.log(addNumbers(5, 3));
+printArray([1, 2, 3])
 
 /* Egzersiz-4
--Bir dikdörtgenin alanı şu şekilde hesaplanır: alan = uzunluk x genişlik. Dikdörtgenin alanını hesaplayan bir fonksiyon yazın.
+-Zamanı bu biçimde gösteren showDateTime adında bir işlev yazın: 08/01/2020 04:08 Date nesnesini kullanarak.
+
+    showDateTime()
+    08/01/2020 04:08
 */
-function alanHesaplama(uzunluk, genislik) {
-    return uzunluk * genislik
+function showDateTime() {
+    let tarih = new Date();
+    let yil = tarih.getFullYear();
+    let ay = tarih.getMonth() + 1;
+    let gun = tarih.getDate();
+    let saat = tarih.getHours();
+    let dakika = tarih.getMinutes();
+    console.log(`${gun}/${ay}/${yil} ${saat}:${dakika}`);
 }
-console.log(alanHesaplama(5, 4));
+showDateTime()
 
 /* Egzersiz-5
--Bir dikdörtgenin çevresi şu şekilde hesaplanır: çevre= 2x(uzunluk + genişlik). Dikdörtgenin çevresini hesaplayan bir fonksiyon yazınız.
+-swapValues adında bir fonksiyon tanımlayın. Bu fonksiyon x değerini y ile değiştirir.
+
+    swapValues(3, 4) // x => 4, y=>3
+    swapValues(4, 5) // x = 5, y = 4
 */
-function cevreHesaplama(uzunluk, genislik) {
-    return 2 * uzunluk * genislik
+function swapValues(x, y) {
+    let newX = y
+    let newY = x
+    return { x: newX, y: newY }
 }
-console.log(cevreHesaplama(5, 4));
+console.log(swapValues(1, 2));
 
 /* Egzersiz-6
--Bir dikdörtgenler prizmasının hacmi şu şekilde hesaplanır: hacim = uzunluk x genişlik x yükseklik. DikdörtgenPrizma hacmini hesaplayan bir fonksiyon yazınız.
+-reverseArray adında bir fonksiyon tanımlayın. Parametre olarak dizi alır ve dizinin tersini döndürür (method kullanmayın).
+
+    console.log(reverseArray([1, 2, 3, 4, 5]))
+    //[5, 4, 3, 2, 1]
+    console.log(reverseArray(['A', 'B', 'C']))
+    //['C', 'B', 'A']
 */
-function prizmaHacim(uzunluk, genislik, yukseklik) {
-    return uzunluk * genislik * yukseklik
+function reverseArray(dizi) {
+    let reverseArray = []
+    for (let i = 0; i < dizi.length; i++) {
+        reverseArray[i] = dizi[dizi.length - 1 - i]
+    }
+    return reverseArray
 }
-console.log(prizmaHacim(5, 4, 3));
+console.log(reverseArray([1, 2, 3, 4, 5, 6, 7]));
 
 /* Egzersiz-7
--Bir dairenin alanı şu şekilde hesaplanır: alan = π x r x r. Dairenin alanını hesaplayan bir fonksiyon yazınız.
+-capitalizeArray adında bir fonksiyon tanımlayın. Parametre olarak array alır ve - capitalizedarray değerini döndürür.
 */
-function daireAlan(yaricap) {
-    const PI = Math.PI
-    return PI * yaricap * yaricap
+function capitalizeArray(dizi) {
+    var capitalizedArray = [];
+
+    for (var i = 0; i < dizi.length; i++) {
+        if (typeof dizi[i] === 'string') {
+            capitalizedArray.push(dizi[i].charAt(0).toUpperCase() + dizi[i].slice(1));
+        } else {
+            capitalizedArray.push(dizi[i]);
+        }
+    }
+
+    return capitalizedArray;
 }
-console.log(daireAlan(5));
+console.log(capitalizeArray(["apple", "banana"]));
 
 /* Egzersiz-8
--Bir dairenin çevresi şu şekilde hesaplanır: çevre = 2πr. Dairenin çevresini hesaplayan bir fonksiyon yazın
+-addItem adında bir fonksiyon tanımlayın. Parametre olarak bir öğe alır ve öğe eklendikten sonra bir dizi döndürür
 */
-function daireCevresi(yaricap) {
-    const PI = Math.PI
-    return 2 * PI * yaricap
+var defaultArray = [1, 2, 3]
+function addItem(newItem, arr) {
+    arr[arr.length] = newItem
+    return arr
 }
-console.log(daireCevresi(5));
+console.log(addItem(4, defaultArray));
 
 /* Egzersiz-9
--Bir maddenin yoğunluğu şu şekilde hesaplanır: yoğunluk= kütle/hacim. Yoğunluğu hesaplayan bir fonksiyon yazınız.
+-removeItem adında bir fonksiyon tanımlayın. Bir indeks parametresi alır ve bir öğeyi kaldırdıktan sonra bir dizi döndürür
 */
-function daireCevresi(yaricap) {
-    const PI = Math.PI
-    return 2 * PI * yaricap
+var defaultArray = [1, 2, 3]
+function removeItem(item, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == item) {
+            arr.splice(i, 1)
+        }
+    }
+    return arr
 }
-console.log(daireCevresi(5));
+console.log(removeItem(2, defaultArray));
 
 /* Egzersiz-10
--Hız, hareket eden bir cismin kat ettiği toplam mesafenin geçen toplam süreye bölünmesiyle hesaplanır. Hareket eden bir nesnenin hızını hesaplayan bir fonksiyon yazınız, hız.
+-sumOfNumbers adında bir fonksiyon tanımlayın. Bir sayı parametresi alır ve bu aralıktaki tüm sayıları toplar.
 */
-function hizHesaplama(mesafe, sure) {
-    let hiz = mesafe / sure
-    return hiz
+function sumOfNumbers(aralik) {
+    let result = 0
+    for (let i = 0; i <= aralik; i++) {
+        result += i
+    }
+    return result
 }
-console.log(hizHesaplama(10, 5));
+console.log(sumOfNumbers(5));
 
 /* Egzersiz-11
--Bir maddenin ağırlığı şu şekilde hesaplanır: ağırlık = kütle x yerçekimi. Ağırlığı hesaplayan bir fonksiyon yazınız.
+-sumOfOdds adında bir fonksiyon tanımlayın. Bir sayı parametresi alır ve bu aralıktaki tüm tek sayıları toplar.
 */
-function agirlikHesaplama(kutle) {
-    const yercekimi = 9.81
-    let agirlik = kutle * yercekimi
-    return agirlik
+function sumOfOdds(aralik) {
+    let result = 0
+    for (let i = 0; i <= aralik; i++) {
+        i % 2 !== 0 ? result += i : null
+    }
+    return result
 }
-console.log(agirlikHesaplama(10));
+console.log(sumOfOdds(5));
 
 /* Egzersiz-12
--oC cinsinden sıcaklık şu formül kullanılarak oF'ye dönüştürülebilir: oF = (oC x 9/5) + 32. oC'yi oF'ye dönüştüren bir fonksiyon yazınız. convertCelsiusToFahrenheit.
+-sumOfEven adında bir fonksiyon tanımlayın. Bir sayı parametresi alır ve bu aralıktaki tüm çift sayıları toplar.
 */
-function dereceDonustur(celcius) {
-    let fahrenheit = (celcius * (9 / 5)) + 32
-    return fahrenheit
+function sumOfEven(aralik) {
+    let result = 0
+    for (let i = 0; i <= aralik; i++) {
+        i % 2 == 0 ? result += i : null
+    }
+    return result
 }
-console.log(dereceDonustur(20));
+console.log(sumOfEven(6));
 
 /* Egzersiz-13
--Vücut kitle indeksi (BMI) şu şekilde hesaplanır: bmi = Kg cinsinden ağırlık / m2 cinsinden (boy x boy). BMI'yi hesaplayan bir fonksiyon yazınız. BMI, 20 yaş ve üzeri yetişkinlerde farklı kilo gruplarını genel olarak tanımlamak için kullanılır. Aşağıda verilen bilgilere dayanarak bir kişinin zayıf, normal, aşırı kilolu veya obez olup olmadığını kontrol edin.
-
-    -Aynı gruplar hem erkekler hem de kadınlar için geçerlidir.
-    -Zayıf: VKİ 18,5'in altındadır
-    -Normal kilolu: BMI 18,5 ila 24,9 arasındadır
-    -Aşırı kilolu: BMI 25 ila 29,9 arasındadır
-    -Obez: BMI 30 veya daha fazladır
+-evensAndOdds adında bir fonksiyon tanımlayın.Parametre olarak pozitif bir tamsayı alır ve sayıdaki çiftlerin ve teklerin sayısını sayar.
+    evensAndOdds(100);
+    Teklerin sayısı 50'dir.
+    Çiftlerin sayısı 51'dir.
 */
-function vucutKitleIndeksi(agirlik, boy) {
-    let bmi = agirlik / (boy * boy)
-    console.log(bmi);
-    if (bmi >= 30) {
-        console.log("Obezsiniz");
-    } else if (bmi < 30 && bmi >= 25) {
-        console.log("Aşırı kilolusunuz");
-    } else if (bmi < 25 && bmi >= 18) {
-        console.log("Normal kilonuzdasınız");
-    } else if (bmi < 18) {
-        console.log("Zayıfsınız");
-    } else {
-        console.log("Lütfen geçerli değerler giriniz(kilonuzu kg, boyunuzu metre cinsinden yazınız");
+function evensAndOdds(aralik) {
+    let evens = 0
+    let odds = 0
+    for (let i = 0; i <= aralik; i++) {
+        i % 2 !== 0 ? odds += 1 : evens += 1
     }
+
+    console.log(`Çift sayılar: ${evens}\nTek sayılar: ${odds}`);
 }
-vucutKitleIndeksi(60, 1.77)
+evensAndOdds(100);
 
 /* Egzersiz-14  
--checkSeason adında bir fonksiyon yazın, bir ay parametresi alır ve mevsimi döndürür: Sonbahar, Kış, İlkbahar veya Yaz.
+-Herhangi bir sayıda argüman alan ve argümanların toplamını döndüren bir fonksiyon yazın
+
+    sum(1, 2, 3) // -> 6
+    sum(1, 2, 3, 4) // -> 10
 */
-function checkSeason(girdi) {
-    let ay = girdi.toLocaleLowerCase()
-    if (ay == "eylül" || ay == "ekim" || ay == "kasım") {
-        console.log("Mevsim sonbahar.");
-    } else if (ay == "aralık" || ay == "ocak" || ay == "şubat") {
-        console.log("Mevsim kış.");
-    } else if (ay == "mart" || ay == "nisan" || ay == "mayıs") {
-        console.log("Mevsim ilkbahar.");
-    } else if (ay == "haziran" || ay == "temmuz" || ay == "ağustos") {
-        console.log("Mevsim yaz.");
-    } else {
-        console.log("Lütfen bir mevsim giriniz!");
+function sumOfRandomNumbers() {
+    let result = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        result += arguments[i]
     }
+    return result
 }
-checkSeason("Mayıs")
+console.log(sumOfRandomNumbers(1, 2, 3));
 
 /* Egzersiz-15
--Math.max en büyük argümanını döndürür. Üç argüman alan ve Math.max yöntemini kullanmadan bunların maksimumunu döndüren findMax adlı bir fonksiyon yazın.
+-RandomUserIp üreten bir fonksiyon yazınız.
 */
-function findMax() {
-    let maxSayi = Math.max(...arguments)
-    return maxSayi
+function RandomUserIp() {
+    var octet1 = Math.floor(Math.random() * 256);
+    var octet2 = Math.floor(Math.random() * 256);
+    var octet3 = Math.floor(Math.random() * 256);
+    var octet4 = Math.floor(Math.random() * 256);
+
+    var ipAddress = octet1 + '.' + octet2 + '.' + octet3 + '.' + octet4;
+
+    return ipAddress;
 }
-console.log(findMax(1,3,5,-1,7));
+console.log(RandomUserIp());
+
+/* Egzersiz-16
+-Rastgele bir macAddress üreten bir fonksiyon yazın
+*/
+function getRandomHexByte() {
+    var randomByte = Math.floor(Math.random() * 256); // 0-255 arasında rastgele bir sayı
+    var hexString = randomByte.toString(16); // Sayıyı hexadecimal formata çevir
+
+    // Hexadecimal stringi iki haneli yapmak için gerekirse başına 0 ekle
+    if (hexString.length < 2) {
+        hexString = '0' + hexString;
+    }
+
+    return hexString;
+}
+
+function macAddress() {
+    var macPart1 = getRandomHexByte();
+    var macPart2 = getRandomHexByte();
+    var macPart3 = getRandomHexByte();
+    var macPart4 = getRandomHexByte();
+    var macPart5 = getRandomHexByte();
+    var macPart6 = getRandomHexByte();
+
+    var macAddress = macPart1 + ':' + macPart2 + ':' + macPart3 + ':' +
+        macPart4 + ':' + macPart5 + ':' + macPart6
+    return macAddress;
+
+}
+console.log(macAddress());
+
+/* Egzersiz-17
+-randomHexaNumberGenerator adında bir fonksiyon tanımlayın.Bu fonksiyon çağrıldığında rastgele bir onaltılık sayı üretir. İşlev onaltılık sayıyı döndürür.
+
+    console.log(randomHexaNumberGenerator());
+    '#ee33df'
+*/
+function randomHexaNumberGenerator() {
+    let hexadecimal = ['#']
+    for (let i = 1; i <= 6; i++) {
+        let randomHexa = Math.floor(Math.random() * 16).toString(16)
+        hexadecimal.push(randomHexa)
+    }
+    console.log(hexadecimal.join(''));
+}
+randomHexaNumberGenerator()
+
+/* Egzersiz-18
+-userIdGenerator adında bir fonksiyon tanımlayın. Bu fonksiyon çağrıldığında yedi karakterlik bir kimlik oluşturur. İşlev kimliği döndürür.
+
+    console.log(userIdGenerator());
+    41XTDbE
+*/
+function userIdGenerator() {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    var userId = '';
+
+    for (var i = 0; i < 7; i++) {
+        var randomIndex = Math.floor(Math.random() * characters.length);
+        userId += characters.charAt(randomIndex);
+    }
+
+    return userId;
+}
+console.log(userIdGenerator());
+
