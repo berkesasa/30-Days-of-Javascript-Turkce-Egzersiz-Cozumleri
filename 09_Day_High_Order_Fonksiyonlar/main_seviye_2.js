884a06b9d37d5e797ed577473ac55af92541e56b
berkesasa <berkesasa11@gmail.com>
2023-12-19 21:53:08 +0300

    created exercise 2

--- 09_Day_High_Order_Fonksiyonlar/main_seviye_2.js
+++ 09_Day_High_Order_Fonksiyonlar/main_seviye_2.js
@@ -1,1 +1,31 @@
 // Seviye-2
+
+/* Egzersiz-1
+-İki veya daha fazla dizi yineleyicisini zincirleyerek ürünlerin toplam fiyatını bulun (örn. arr.map(callback).filter(callback).reduce(callback))
+*/
+
+
+/* Egzersiz-2
+-Yalnızca reduce reduce(callback)) kullanarak ürünlerin fiyatlarının toplamını bulun
+*/
+
+/* Egzersiz-3
+-Bazı ortak kalıplara sahip ülkelerden oluşan bir dizi döndüren categorizeCountries adlı bir fonksiyon tanımlayın (ülkeler dizisini bu depoda countries.js olarak bulabilirsiniz (örneğin 'land', 'ia', 'island','stan')).
+*/
+
+/* Egzersiz-4
+-Harf ve harfin bir ülke adıyla başlamak için kaç kez kullanıldığını gösteren bir dizi nesne döndüren bir fonksiyon oluşturun.
+*/
+
+/* Egzersiz-5
+-Bir getFirstTenCountries fonksiyonu tanımlayın ve on ülkeden oluşan bir dizi döndürün. countries.js dizisi üzerinde çalışmak için farklı işlevsel programlama kullanın
+*/
+
+/* Egzersiz-6
+-countries dizisindeki son on ülkeyi döndüren bir getLastTenCountries fonksiyonu tanımlayın.
+*/
+
+/* Egzersiz-7
+-Ülkeler dizisinden bir ülke adının baş harfi olarak en çok hangi harfin kullanıldığını bulun (örn. Finlandiya, Fiji, Fransa vb.)
+*/
+
