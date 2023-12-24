a3e9d7b5b3b72895ef03183a0c0156764b4ecebb
berkesasa <berkesasa11@gmail.com>
2023-12-24 17:13:19 +0300

    finished exercise-2

---
+++ 11_Day_Destructuring_ve_Spreading/main_seviye_2.js
@@ -0,0 +1,72 @@
+// Seviye-2
+
+const constants = [2.72, 3.14, 9.81, 37, 100]
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const rectangle = {
+    width: 20,
+    height: 10,
+    area: 200,
+    perimeter: 60
+}
+const users = [
+    {
+        name: 'Brook',
+        scores: 75,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 16
+    },
+    {
+        name: 'Alex',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'David',
+        scores: 75,
+        skills: ['HTM', 'CSS'],
+        age: 22
+    },
+    {
+        name: 'John',
+        scores: 85,
+        skills: ['HTML'],
+        age: 25
+    },
+    {
+        name: 'Sara',
+        scores: 95,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name: 'Martha',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'Thomas',
+        scores: 90,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 20
+    }
+]
+
+/* Egzersiz-1
+-users dizisini Iterator ve destructuring kullanarak nesnenin tüm anahtarlarını alın.
+*/
+for(const {name, scores, skills, age} of users){
+    console.log({name, scores, skills, age});
+}
+
+/* Egzersiz-2
+-İkiden az beceriye sahip kişileri bulun.
+*/
+for(const {name, scores, skills, age} of users){
+    if(skills.length <= 2){
+        console.log({name, scores, skills, age});
+    }
+}
+
+
