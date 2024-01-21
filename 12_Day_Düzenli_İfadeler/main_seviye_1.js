bba8230d8e2f461185428abc4b4ad7814c89c0eb
berkesasa <berkesasa11@gmail.com>
2024-01-21 20:22:32 +0300

    created Day 12 level1 questions

--- 12_Day_Düzenli_İfadeler/main_seviye_1.js
+++ 12_Day_Düzenli_İfadeler/main_seviye_1.js
@@ -1,1 +1,22 @@
 // Seviye-1
+
+/* Egzersiz-1
+-Aşağıdaki metinden kişinin toplam yıllık gelirini hesaplayınız.
+'Ayda 4000 euro maaş, 10000 euro yıllık ikramiye, ayda 5500 euro online kurs kazanıyor.'
+*/
+
+/* Egzersiz-2
+-Yatay x ekseni üzerindeki bazı parçacıkların konumu negatif yönde -12, -4, -3 ve -1, orijinde 0, pozitif yönde 4 ve 8'dir. Bu sayıları çıkarın ve en uzak iki parçacık arasındaki mesafeyi bulun.
+    points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+    sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8]
+    distance = 12
+
+*/
+
+/* Egzersiz-3
+-Bir dizenin geçerli bir JavaScript değişkeni olup olmadığını belirleyen bir kalıp yazın
+    is_valid_variable('first_name') # True
+    is_valid_variable('first-name') # False
+    is_valid_variable('1first_name') # False
+    is_valid_variable('firstname') # True
+*/
